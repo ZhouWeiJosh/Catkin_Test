15c2dd1f0c25f23446df019338d49dc01f2dcf91
ZhouWeiJosh <joshuachowishere@gmail.com>
2023-11-17 17:49:19 -1000

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/joshua/robot_practice_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/joshua/robot_practice_ws/devel;/opt/ros/melodic'.split(';') if '/home/joshua/robot_practice_ws/devel;/opt/ros/melodic' != '' else []
